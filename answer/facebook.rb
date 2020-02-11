ddb335a8120d13881714f7bb78ef4f938a83797d
om-hondaseiha <honda.seiha.infratop@gmail.com>
2020-02-11 16:02:58 +0900

    facebook解答追記

--- answer/facebook.rb
+++ answer/facebook.rb
@@ -11,3 +11,21 @@ i=1
 	end
 	i = i + 1
 end
+
+# arr = [*1..100]
+# arr.each do |num|
+# for num in 1..100 do
+# num = 1
+# # while num < 101
+# 100.times do
+# 	if num % 15 == 0
+# 		puts 'facebook'
+# 	elsif num % 3 == 0
+# 		puts 'face'
+# 	elsif num % 5 == 0
+# 		puts 'book'
+# 	else
+# 		puts num.to_s
+# 	end
+# 	num += 1
+# end
