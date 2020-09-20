f736791f1a37bd9e4aa8f30589f18f8942260a4a
kbc18a11 <wada121400@gmail.com>
2020-09-20 16:53:37 +0900

    初回コミット

--- facebook.rb
+++ facebook.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # 「FaceBook」
 # 以下の条件に従って、1から100までの数字を出力しましょう。
 # - 条件
@@ -5,3 +7,35 @@
 #  - 値が5で割り切れるときは'book'と出力する。
 #  - 値が15で割り切れるときは'facebook'と出力する。
 #  - それ以外の場合、値をそのまま出力する。
+
+# 出力の判定
+# @param num 割られる数
+def divisioning(num)
+  # 3で割り切れるか?
+  puts 'face' if num % 3 == 0
+
+  # 5で割り切れるか?
+  puts 'book' if num % 5 == 0
+
+  # 15で割り切れるか?
+  if num % 15 == 0
+    puts 'facebook'
+
+  # 3，5，15どれも割り切れなかったか?
+  else
+    puts num
+  end
+end
+
+loop do
+  puts '1から100数字を入力'
+  # 割られる数を入力
+  num = gets.to_i
+
+  if num > 100
+    puts '1から100数字を入力してください'
+    next
+  end
+
+  divisioning(num)
+end
--- janken.rb
+++ janken.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # じゃんけんをして、勝ち負けを判定させましょう。（相手の出す手は乱数で決めましょう）
 #  以下は出力例です。
 #  -----------------
@@ -22,3 +24,108 @@
 # [1]:チョキ
 # [2]:パー
 # ...
+
+# 勝利時のメッセージ
+def win_message
+  puts 'あなたの勝ちです'
+end
+
+# 引き分け時のメッセージ
+def draw_messsage
+  puts '引き分けです'
+end
+
+# 敗北時のメッセージ
+def lost_message
+  puts 'あなたの負けです'
+end
+
+# メニュー表示
+def plint_menu
+  puts '-----------------'
+  puts '最初はグー、じゃんけん...'
+  puts '[0]:グー'
+  puts '[1]:チョキ'
+  puts '[2]:パー'
+end
+
+# じゃんけんの判定を行う
+# @param player プレイヤーの手
+# @param cp 相手の手
+def game_judgment(player, cp)
+  puts "あなたの手:#{player}、相手の手:#{cp}"
+
+  # プレイヤー手ごとの勝負の判定を分岐させる
+  case player
+  when 'グー'
+    player_case_goo(cp)
+  when 'チョキ'
+    player_case_choki(cp)
+  when 'パー'
+    player_case_paa(cp)
+  end
+end
+
+# プレイヤーがグーだった場合の判定
+# @param cp 相手の手
+def player_case_goo(cp)
+  # 相手の出した手の判定によって表示結果を変更
+  case cp
+  when 'グー'
+    puts draw_messsage
+  when 'チョキ'
+    puts win_message
+  when 'パー'
+    puts lost_message
+  end
+end
+
+# プレイヤーがチョキだった場合の判定
+# @param cp 相手の手
+def player_case_choki(cp)
+  # 相手の出した手の判定によって表示結果を変更
+  case cp
+  when 'グー'
+    puts lost_message
+  when 'チョキ'
+    puts draw_messsage
+  when 'パー'
+    puts win_message
+  end
+end
+
+# プレイヤーがパーだった場合の判定
+# @param cp 相手の手
+def player_case_paa(cp)
+  # 相手の出した手の判定によって表示結果を変更
+  case cp
+  when 'グー'
+    puts win_message
+  when 'チョキ'
+    puts lost_message
+  when 'パー'
+    puts draw_messsage
+  end
+end
+
+# 手の種類
+HAND_TYPE = %w[グー チョキ パー].freeze
+
+loop do
+  # メニューを表示
+  plint_menu
+  # プレイヤーの手
+  player = HAND_TYPE[gets.to_i]
+
+  # 0~2以外の数値が入力されたか?
+  unless player
+    puts '入力された値が無効です'
+    next
+  end
+
+  # 対戦相手の手
+  cp = HAND_TYPE[rand(3)]
+
+  # 勝負の判定を行う
+  game_judgment(player, cp)
+end
